4f60866541b9ed57638ead330f5b69aa0b7d4983
KUMARANKIT3012 <ankitkumar30122004@gmail.com>
2026-07-17 22:22:08 +0530

    Update document editor page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -12,7 +12,6 @@ import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
 // import { Editor } from "@/components/editor";
-import {Chatbot} from "@/components/chatbot";
 
 interface DocumentIdPageProps {
   params: Promise<{
